3b0e8dcf52d9d427d502a366cf35281370b1c5ad
Per Øyvind Karlsen <proyvind@moondrake.org>
2016-11-13 05:58:15 +0000

    use local errbit

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -2,7 +2,7 @@ require 'airbrake'
 
 Airbrake.configure do |config|
   config.api_key = APP_CONFIG['keys']['airbrake_api_key']
-  config.host    = 'errbit.rosalinux.ru'
-  config.port    = 80
+  config.host    = 'localhost'
+  config.port    = 8080
   config.secure  = config.port == 443
 end
