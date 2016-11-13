156f0e9911c299a720f4faf7c0debe7ce7b06992
Per Øyvind Karlsen <proyvind@moondrake.org>
2016-11-13 10:46:28 +0000

    change airbrake port

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -3,6 +3,6 @@ require 'airbrake'
 Airbrake.configure do |config|
   config.api_key = APP_CONFIG['keys']['airbrake_api_key']
   config.host    = 'localhost'
-  config.port    = 8080
+  config.port    = 8008
   config.secure  = config.port == 443
 end
